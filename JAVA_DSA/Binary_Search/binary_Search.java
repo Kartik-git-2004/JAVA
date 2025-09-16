6bdebfd02df4d187eb70559c00ffbf8bd11b151e
Kartik-git-2004 <kartiksharmapmsss@gmail.com>
2025-09-16 20:32:11 +0000

    correted version of binary search

--- JAVA_DSA/Binary_Search/binary_Search.java
+++ JAVA_DSA/Binary_Search/binary_Search.java
@@ -1,35 +1,43 @@
-public class binary_Search{
-    public static void main (String[] args){
-     int [] arr ={-9,-8,-4,1,2,5,6,7,9,14,15,117};
+// order-agnostic Binary Search .. if we dont know how array is sorted in ascending or in descending ...
+class Main {
+    public static void main(String[] args) {
+             int [] arr ={-9,-8,-4,1,2,5,6,7,9,14,15,117};
 int result= binarySearch(arr,14);
 System.out.println(result);
-        
     }
-
-    public static int binarySearch(int[] arr, int target){
-        int start = 0;
+    
+    public static int binarySearch(int [] arr, int target ){
+        int start =0;
         int end = arr.length-1;
         
-
-        // return -1 if it doesnot exist ..
         while(start<=end){
-            //find the element 
-            int mid  = start +(end-start)/2;
-
-            if(target<arr[mid]){
-                end = mid-1;
+            
+            if(arr[start]<arr[end]){
+                int mid = start+(end-start)/2;
+                if(target<mid){
+                    end= mid-1;
+                }
+                else if(target>arr[mid]){
+                    start= mid+1;
+                }else{
+                    return mid;
+                }
             }
-            if(target>arr[mid]){
-                start= mid+1;
-
+            
+           else if(arr[start]>arr[end]){
+                int mid =  start+(end-start)/2;
+                if(target<mid){
+                    start=mid+1;
+                }
+                else if(target>arr[mid]){
+                    end=mid-1;
+                }
+                else{
+                    return mid; 
+                }
             }
-            if(target==arr[mid]){
-                return mid;
-            }
-
+         
         }
-
-        return -1 ;
+          return -1;
     }
-
 }
