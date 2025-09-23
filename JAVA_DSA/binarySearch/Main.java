87ce9489f11fdb70fcac8e9a5515bad8050163e6
Kartik-git-2004 <kartiksharmapmsss@gmail.com>
2025-09-23 17:04:36 +0000

    floor

---
+++ JAVA_DSA/binarySearch/Main.java
@@ -0,0 +1,36 @@
+// celing of Mainber
+
+public class Main {
+    public static void main(String[] args) {
+
+        int [] arr= {1,2,36,77,88,99,110,112,113,115};
+        int res = celings(arr,78);
+        System.out.println(res+":");
+
+    }
+    public  static int celings(int [] x, int target){
+        int left = 0, right=x.length-1;
+        
+       
+        
+        while(left<=right){
+            
+            int mid = left+(right-left)/2;
+            
+            
+            if(x[mid]==target){
+                return x[mid];
+            }
+            if(target>x[mid]){
+                left=mid+1;
+            }else {
+                right=mid-1;
+            }
+        }
+         if (left == x.length) {   
+             return -1;           
+           }
+        return x[right];
+
+    }
+}
