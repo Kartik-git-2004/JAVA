ee39a7092004eb7309b6b7e7a3cc88b65430eb5b
Kartik-git-2004 <kartiksharmapmsss@gmail.com>
2025-09-23 03:30:29 +0000

    // celing of a number

--- JAVA_DSA/binarySearch/celing_of_number.java
+++ JAVA_DSA/binarySearch/celing_of_number.java
@@ -1,9 +1,36 @@
-class Main {
+// celing of a number
 
-    public static void Main(String[] args) {
-        int[] arr = {2, 3, 5, 9, 14, 16, 18};
-        int target = 15;
-        int ans = ceiling(arr, target);
-        System.out.println(ans);
+public class Main {
+    public static void main(String[] args) {
+
+        int [] arr= {1,2,36,77,88,99,110,112,113,115};
+        int res = celings(arr,78);
+        System.out.println(res+":");
+
+    }
+    public  static int celings(int [] x, int target){
+        int left = 0, right=x.length-1;
+        
+       
+        
+        while(left<=right){
+            
+            int mid = left+(right-left)/2;
+            
+            
+            if(x[mid]==target){
+                return x[mid];
+            }
+            if(target>x[mid]){
+                left=mid+1;
+            }else {
+                right=mid-1;
+            }
+        }
+         if (left == x.length) {   
+             return -1;           
+           }
+        return x[left];
+
     }
 }
